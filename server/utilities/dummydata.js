6de0e249e13182d1fe13857af77aa0a83f1c8d70
Chigozie Asadu <45165481+ChigozieA@users.noreply.github.com>
2019-07-05 08:30:42 +0100

    chore(dummy data): generate dummy data
    [Delivers #167100654]

---
+++ server/utilities/dummydata.js
@@ -0,0 +1,144 @@
+export default {
+    users:[
+        [
+            {
+              "id": 0,
+              "email": "fordconway@accel.com",
+              "first_name": "Elinor",
+              "last_name": "<ReferenceError: lastName is not defined>",
+              "password": "whufu",
+              "phone": "+234 (896) 475-2941",
+              "address": "536 Garfield Place, Williamson, Virgin Islands, 7890",
+              "is_admin": true
+            },
+            {
+              "id": 1,
+              "email": "elinorconway@accel.com",
+              "first_name": "Green",
+              "last_name": "<ReferenceError: lastName is not defined>",
+              "password": "dftrg",
+              "phone": "+234 (986) 453-2903",
+              "address": "170 Columbia Place, Highland, Pennsylvania, 3728",
+              "is_admin": true
+            },
+            {
+              "id": 2,
+              "email": "greenconway@accel.com",
+              "first_name": "Maryann",
+              "last_name": "<ReferenceError: lastName is not defined>",
+              "password": "dftrg",
+              "phone": "+234 (811) 478-3389",
+              "address": "756 Wyona Street, Epworth, Guam, 5077",
+              "is_admin": false
+            },
+            {
+              "id": 3,
+              "email": "maryannconway@accel.com",
+              "first_name": "Paul",
+              "last_name": "<ReferenceError: lastName is not defined>",
+              "password": "njghy",
+              "phone": "+234 (920) 480-3711",
+              "address": "605 Manhattan Court, Darbydale, Wisconsin, 1958",
+              "is_admin": false
+            },
+            {
+              "id": 4,
+              "email": "paulconway@accel.com",
+              "first_name": "Malone",
+              "last_name": "<ReferenceError: lastName is not defined>",
+              "password": "gteyu",
+              "phone": "+234 (840) 495-2025",
+              "address": "643 Ralph Avenue, Watchtower, California, 2220",
+              "is_admin": false
+            }
+          ]
+    ],
+    properties:[
+        [
+            {
+              "id": 0,
+              "owner": 24,
+              "status": "sold",
+              "price": "$1,919.79",
+              "city": "Ada",
+              "state": "Tennessee",
+              "address": "812 Berriman Street, Stagecoach, Oregon, 984",
+              "type": "studio apartment",
+              "created_on": "2019-03-29T01: 52:02 -01:00",
+              "image_url": "http://placehold.it/32x32"
+            },
+            {
+              "id": 1,
+              "owner": 30,
+              "status": "not sold",
+              "price": "$3,753.68",
+              "city": "Keyport",
+              "state": "Michigan",
+              "address": "411 Dodworth Street, Eagletown, Massachusetts, 7912",
+              "type": "miniflat",
+              "created_on": "2019-06-18T10: 09:13 -01:00",
+              "image_url": "http://placehold.it/32x32"
+            },
+            {
+              "id": 2,
+              "owner": 34,
+              "status": "sold",
+              "price": "$2,394.23",
+              "city": "Navarre",
+              "state": "Wyoming",
+              "address": "621 Evergreen Avenue, Freeburn, North Carolina, 8608",
+              "type": "2 Bedroom flat",
+              "created_on": "2019-04-30T05: 31:14 -01:00",
+              "image_url": "http://placehold.it/32x32"
+            },
+            {
+              "id": 3,
+              "owner": 27,
+              "status": "not sold",
+              "price": "$3,432.30",
+              "city": "Mapletown",
+              "state": "American Samoa",
+              "address": "422 Beayer Place, Leola, Nevada, 3091",
+              "type": "3 Bedroom flat",
+              "created_on": "2019-05-17T07: 40:11 -01:00",
+              "image_url": "http://placehold.it/32x32"
+            },
+            {
+              "id": 4,
+              "owner": 40,
+              "status": "not sold",
+              "price": "$2,735.76",
+              "city": "Richmond",
+              "state": "Oklahoma",
+              "address": "972 Dennett Place, Devon, Federated States Of Micronesia, 739",
+              "type": "studio apartment",
+              "created_on": "2019-01-27T01: 29:09 -01:00",
+              "image_url": "http://placehold.it/32x32"
+            },
+            {
+              "id": 5,
+              "owner": 35,
+              "status": "not sold",
+              "price": "$1,087.64",
+              "city": "Witmer",
+              "state": "Texas",
+              "address": "882 Poly Place, Roulette, Maryland, 7170",
+              "type": "3 Bedroom flat",
+              "created_on": "2019-07-01T02: 52:31 -01:00",
+              "image_url": "http://placehold.it/32x32"
+            },
+            {
+              "id": 6,
+              "owner": 33,
+              "status": "not sold",
+              "price": "$1,701.98",
+              "city": "Wintersburg",
+              "state": "Delaware",
+              "address": "558 Visitation Place, Clarence, Virgin Islands, 4581",
+              "type": "miniflat",
+              "created_on": "2019-05-21T08: 54:30 -01:00",
+              "image_url": "http://placehold.it/32x32"
+            }
+          ]
+    ]
+}
